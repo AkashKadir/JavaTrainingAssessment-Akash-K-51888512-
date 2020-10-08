7d45e7b5ee8c79229f6567c780b23394476fa6d8
AkashKadir <64208425+AkashKadir@users.noreply.github.com>
2020-10-08 10:01:38 +0530

    Update Confirm.java

--- Session and Cookie Hands-on/Sesandcook2/src/org/ticket/controller/Confirm.java
+++ Session and Cookie Hands-on/Sesandcook2/src/org/ticket/controller/Confirm.java
@@ -32,6 +32,13 @@ public class Confirm extends HttpServlet {
 		else if(ticketType.equals("Bronze")) {
 			cost = 100*numberOfTicket;
 		}
+		Integer costInt = cost;
+		Cookie seatType = new Cookie("seatType",ticketType);
+		Cookie numberOfTickets = new Cookie("numberOfTickets",noOfTicket);
+		Cookie ticketCost = new Cookie("ticketCost",costInt.toString());
+		res.addCookie(seatType);
+		res.addCookie(numberOfTickets);
+		res.addCookie(ticketCost);
 		pw.println("<h1>Ticket Confirmation</h1>");
 		pw.println("<div>The Ticket Cost is "+cost+"</div>");
 		pw.println("<p>Do you want to confirm your ticket?</p>");
@@ -39,6 +46,7 @@ public class Confirm extends HttpServlet {
 		pw.println("<input type=\"submit\" value=\"Confirm\"></form>");
 		pw.println("<form action=\"./Index\" method=\"GET\">");
 		pw.println("<input type=\"submit\" value=\"Cancel\"></form>");
+		pw.close();
 	}
 
 }
